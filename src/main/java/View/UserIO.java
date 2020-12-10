7df63d2921cfdbfef920909f83c1a81bf6c4e16b
Larisa Gargalic <lgargalic@gmail.com>
2020-12-10 16:23:38 -0500

    * add file with some data

--- src/main/java/View/UserIO.java
+++ src/main/java/View/UserIO.java
@@ -1,11 +1,14 @@
 package View;
 
 public interface UserIO {
+
     void print(String msg);
 
-    double readDouble(String prompt);    
+    double readDouble(String prompt);
 
-    int readInt(String prompt);    
+    int readInt(String prompt);
 
-    String readString(String prompt);    
+    int readInt(String prompt, int min, int max);
+
+    String readString(String prompt);
 }
