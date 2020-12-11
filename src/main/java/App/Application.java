9bd746757a38ee081e93fbfd7e56e7c84f69f4ff
Larisa Gargalic <lgargalic@gmail.com>
2020-12-11 10:50:03 -0500

    * add controller functionality

--- src/main/java/App/Application.java
+++ src/main/java/App/Application.java
@@ -1,5 +1,6 @@
 package App;
 
+import Controller.VendingMachineModelController;
 import DAO.CannotOpenFile;
 import DAO.VendingMachineDAOImplementation;
 import DAO.VendingMachineDAOInterface;
@@ -18,47 +19,13 @@ import java.util.List;
  */
 public class Application {
 
-    public static void main(String[] args) throws CannotOpenFile, InvalidDelimiterException {
-        //        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");       
-        //        VendingMachineModelController controller= context.getBean("controller", VendingMachineModelController.class);
-        //        controller.run();
-
-        //VendingMachineServiceLevel service;
+    public static void main(String[] args) throws CannotOpenFile, InvalidDelimiterException {        
         VendingMachineDAOInterface vendingMachineDao = new VendingMachineDAOImplementation();
         VendingMachineServiceLevel service = new VendingMachineServiceLevelImplementation(vendingMachineDao); 
         UserIO userIo = new UserIOConsoleImpl();
         VendingMachineView view = new VendingMachineView(userIo);
-        UserIO io = new UserIOConsoleImpl();
-        
-        
-        boolean keepGoing = true;
-        int menuSelection = 0;
-        while (keepGoing) {
-            switch (view.printMenuandGetSelection()) {
-                case 1:
-                    view.displayAllProductsBanner();
-                    List<Product> allProducts = vendingMachineDao.getProducts();
-                    view.displayAllProducts(allProducts);
-                    break;
-                case 2:
-                    //manageOrders();
-                    break;
-                case 3:
-                    //displayOrderBook();
-                    break;
-                case 4:
-                    //manageOrders();
-                    break;
-                case 5:
-                    view.exitMessage();
-                    service.shutDown();
-                    keepGoing = false;
-                    break;
-                default:
-                    System.out.println("Unknown Command");
-                    break;
-            }
-        }        
+        VendingMachineModelController controller = new VendingMachineModelController(service,view);
+        controller.run();     
   
 }
 
--- src/main/java/Controller/VendingMachineModelController.java
+++ src/main/java/Controller/VendingMachineModelController.java
@@ -1,9 +1,12 @@
 package Controller;
 
 import DAO.CannotOpenFile;
+import DAO.VendingMachineDAOImplementation;
+import DAO.VendingMachineDAOInterface;
 import DTO.Product;
 import ServiceLayer.InvalidDelimiterException;
 import ServiceLayer.VendingMachineServiceLevel;
+import ServiceLayer.VendingMachineServiceLevelImplementation;
 import View.UserIO;
 import View.UserIOConsoleImpl;
 import View.VendingMachineView;
@@ -12,17 +15,28 @@ import java.util.List;
 public class VendingMachineModelController {
     private VendingMachineServiceLevel service;
     private VendingMachineView view;
-    private UserIO io = new UserIOConsoleImpl();
+//    private UserIO io = new UserIOConsoleImpl();
+
+//    VendingMachineDAOInterface vendingMachineDao;
+//    VendingMachineServiceLevel service = new VendingMachineServiceLevelImplementation(vendingMachineDao);
+//    UserIO userIo = new UserIOConsoleImpl();
+//    VendingMachineView view = new VendingMachineView(userIo);
+//    UserIO io = new UserIOConsoleImpl();
+
     public VendingMachineModelController(VendingMachineServiceLevel service, VendingMachineView view) {
         this.service = service;
         this.view = view;
     }
-    
-    public void run() throws CannotOpenFile, InvalidDelimiterException  {
+
+//    public VendingMachineModelController() {
+//    }   
+
+    public void run() throws CannotOpenFile, InvalidDelimiterException {
+        //vendingMachineDao = new VendingMachineDAOImplementation();
         boolean keepGoing = true;
         int menuSelection = 0;
-        while (keepGoing){
-            switch (getMenuSelection()){
+        while (keepGoing) {
+            switch (getMenuSelection()) {
                 case 1:
                     viewAllTrades();
                     break;
@@ -35,23 +49,26 @@ public class VendingMachineModelController {
                 case 4:
                     //manageOrders();
                     break;
-                case 5: view.exitMessage();
+                case 5:
+                    view.exitMessage();
                     service.shutDown();
                     keepGoing = false;
                     break;
-                default:System.out.println("Unknown Command");
+                default:
+                    System.out.println("Unknown Command");
                     break;
             }
         }
     }
-    private int getMenuSelection(){
+
+    private int getMenuSelection() {
         return view.printMenuandGetSelection();
     }
-    
-    private void viewAllTrades(){
+
+    private void viewAllTrades() {
         view.displayAllProductsBanner();
         List<Product> allProducts = service.getProducts();
         view.displayAllProducts(allProducts);
     }
-    
+
 }
