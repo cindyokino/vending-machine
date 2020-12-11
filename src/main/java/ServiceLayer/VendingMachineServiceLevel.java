fe0a419f8f2b5dab5c045942b08f615f8eee49cf
Larisa Gargalic <lgargalic@gmail.com>
2020-12-10 19:25:51 -0500

    *conflict

--- src/main/java/ServiceLayer/VendingMachineServiceLevel.java
+++ src/main/java/ServiceLayer/VendingMachineServiceLevel.java
@@ -1,20 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ServiceLayer;
 
-<<<<<<< HEAD
-/**
- *
- * @author W
- */
-public interface VendingMachineServiceLevel {    
-    
-=======
 public interface VendingMachineServiceLevel {
->>>>>>> f0893bf855c79b442bd3891bbe88d27eb6f30b03
-    
-    //    amountOfChange function
+
 }
